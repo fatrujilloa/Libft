abd22ddfa16a00d399c19441e3b21b0f2c563773
Frank Trujillo <fatrujilloa@MacBook-Air-de-Frank.local>
2018-11-18 00:14:31 +0100

    more

---
+++ Exec/main_putstr.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main_memcpy.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ftrujill <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/11 13:59:12 by ftrujill          #+#    #+#             */
+/*   Updated: 2018/11/17 18:10:09 by ftrujill         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdio.h>
+
+int		main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		printf("%s. Provide 1 string.\n", argv[0]);
+		return (0);
+	}
+	ft_putstr(argv[1]);
+	ft_putendl(NULL);
+	ft_putendl(argv[1]);
+	return (0);
+}
